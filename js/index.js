9ff407512249b644b7143b309f1c38f1dac09b1b
mpt-workshop <126445663+mpt-workshop@users.noreply.github.com>
2023-02-27 14:23:04 +0800

    SDS website (initial build)

---
+++ js/index.js
@@ -0,0 +1,106 @@
+/*ANTI-RUST*/
+document.querySelector(".item1a .fw-bold").innerHTML = "Biogrease Rust Away";
+document.querySelectorAll(".item1a a")[0].setAttribute("href","");
+document.querySelectorAll(".item1a a")[1].setAttribute("href","https://drive.google.com/file/d/10dqIiXRUSwAH-vGWzzreSPTrPL1MrNum/view?usp=share_link");
+
+document.querySelector(".item1b .fw-bold").innerHTML = "Dr. Rust";
+document.querySelectorAll(".item1b a")[0].setAttribute("href","");
+document.querySelectorAll(".item1b a")[1].setAttribute("href","https://drive.google.com/file/d/1UYPt9k81l9bIL_2TAxwe39MWmnhaKfiE/view?usp=share_link");
+
+document.querySelector(".item1c .fw-bold").innerHTML = "WD-40";
+document.querySelectorAll(".item1c a")[0].setAttribute("href","");
+document.querySelectorAll(".item1c a")[1].setAttribute("href","https://drive.google.com/file/d/1Xr6eTghDr4qMRET2_XBr2Bvh_HT5dKql/view?usp=share_link");
+
+/*DE-GREASER*/
+document.querySelector(".item2a .fw-bold").innerHTML = "AKA 5410 Heavy Duty Rigwash";
+document.querySelectorAll(".item2a a")[0].setAttribute("href","https://drive.google.com/file/d/1uh3oUBpOcVDnz45H2ZfRHUwKhRp6Zmqf/view?usp=share_link");
+document.querySelectorAll(".item2a a")[1].setAttribute("href","https://drive.google.com/file/d/1uh3oUBpOcVDnz45H2ZfRHUwKhRp6Zmqf/view?usp=share_link");
+
+/*DIESEL*/
+document.querySelector(".item3a .fw-bold").innerHTML = "Turbo Diesel Euro 5";
+document.querySelectorAll(".item3a a")[0].setAttribute("href","https://drive.google.com/file/d/1_atuLM5Z0hDJp5ZUYkfplB0uJJyF8l_P/view?usp=share_link");
+document.querySelectorAll(".item3a a")[1].setAttribute("href","https://drive.google.com/file/d/1neENRzx8vLz54Yj59E5vaPwuFxT3xi4l/view?usp=share_link");
+
+/*GREASE*/
+document.querySelector(".item4a .fw-bold").innerHTML = "AMSOIL Semi Synthetic 1.5 460 EP Grease";
+document.querySelectorAll(".item4a a")[0].setAttribute("href","");
+document.querySelectorAll(".item4a a")[1].setAttribute("href","https://drive.google.com/file/d/1gBUOqKDHgzAti_uBrTpxd8h9Y25eGdbq/view?usp=share_link");
+
+/*HAND CLEANER*/
+document.querySelector(".item5a .fw-bold").innerHTML = "Suprega Plus";
+document.querySelectorAll(".item5a a")[0].setAttribute("href","");
+document.querySelectorAll(".item5a a")[1].setAttribute("href","https://drive.google.com/file/d/1KRt2uSJL_loj8jGZyOayJGhg76nAsFdX/view?usp=share_link");
+
+document.querySelector(".item5b .fw-bold").innerHTML = "Swaferga Orange";
+document.querySelectorAll(".item5b a")[0].setAttribute("href","");
+document.querySelectorAll(".item5b a")[1].setAttribute("href","https://drive.google.com/file/d/1qaCcGRATzTJNXbNxCA8Pjm0mnAsVF7kz/view?usp=share_link");
+
+document.querySelector(".item5c .fw-bold").innerHTML = "Swaferga Plus";
+document.querySelectorAll(".item5c a")[0].setAttribute("href","");
+document.querySelectorAll(".item5c a")[1].setAttribute("href","https://drive.google.com/file/d/1sJHIxO4JFGm_pPK9ft5hGq8Z0LuM7RiY/view?usp=share_link");
+
+/*HYDRAULIC OIL*/
+document.querySelector(".item6a .fw-bold").innerHTML = "";
+document.querySelectorAll(".item6a a")[0].setAttribute("href","");
+document.querySelectorAll(".item6a a")[1].setAttribute("href","");
+
+document.querySelector(".item6b .fw-bold").innerHTML = "";
+document.querySelectorAll(".item6b a")[0].setAttribute("href","");
+document.querySelectorAll(".item6b a")[1].setAttribute("href","");
+
+document.querySelector(".item6c .fw-bold").innerHTML = "";
+document.querySelectorAll(".item6c a")[0].setAttribute("href","");
+document.querySelectorAll(".item6c a")[1].setAttribute("href","");
+
+/*PAINT, HARDENER & THINNER*/
+document.querySelector(".item7a .fw-bold").innerHTML = "JOTUN Hardtop XP Comp A (Paint)";
+document.querySelectorAll(".item7a a")[0].setAttribute("href","");
+document.querySelectorAll(".item7a a")[1].setAttribute("href","https://drive.google.com/file/d/13OEjQZHdzGwoihaNqPIUMVIlQyhxOnYn/view?usp=share_link");
+
+document.querySelector(".item7b .fw-bold").innerHTML = "JOTUN Hardtop XP Comp B (Hardener)";
+document.querySelectorAll(".item7b a")[0].setAttribute("href","");
+document.querySelectorAll(".item7b a")[1].setAttribute("href","https://drive.google.com/file/d/1Znbio7IBr2FGMBu12J5dEL25YJpRhCkz/view?usp=share_link");
+
+document.querySelector(".item7c .fw-bold").innerHTML = "JOTUN Thinner No 2 (Thinner)";
+document.querySelectorAll(".item7c a")[0].setAttribute("href","");
+document.querySelectorAll(".item7c a")[1].setAttribute("href","https://drive.google.com/file/d/1_CcAITYJN_9MT1hjcpKpS4RYupl2oXgQ/view?usp=share_link");
+
+document.querySelector(".item7d .fw-bold").innerHTML = "UNY MARINE HS (Hardener)";
+document.querySelectorAll(".item7d a")[0].setAttribute("href","https://drive.google.com/file/d/1wd7f6AB3oTnpsV6rgeR6Q9kFf6tRyYk_/view?usp=share_link");
+document.querySelectorAll(".item7d a")[1].setAttribute("href","https://drive.google.com/file/d/1kHwiswxzsEpRv2X65D1f0ro_1uHRu5Z-/view?usp=share_link");
+
+document.querySelector(".item7e .fw-bold").innerHTML = "UNY MARINE HS Green CS-518 (Paint)";
+document.querySelectorAll(".item7e a")[0].setAttribute("href","https://drive.google.com/file/d/1S-a2adovoNNzs5oGGaYv1kbvNI26LccR/view?usp=share_link");
+document.querySelectorAll(".item7e a")[1].setAttribute("href","https://drive.google.com/file/d/1TyAzNynmJH00K_WcrG2J_CLU9KUnNYKp/view?usp=share_link");
+
+document.querySelector(".item7f .fw-bold").innerHTML = "UNY MARINE HS Red CS-625 (Paint)";
+document.querySelectorAll(".item7f a")[0].setAttribute("href","https://drive.google.com/file/d/1ZuaKz1NwZL3PZc7xnz2-rrvRKQoZycaJ/view?usp=share_link");
+document.querySelectorAll(".item7f a")[1].setAttribute("href","https://drive.google.com/file/d/15zp3y7J3itOlsCyVzC3Wu4QRKeccC9Ok/view?usp=share_link");
+
+/*MISCELLANEOUS*/
+document.querySelector(".item8a .fw-bold").innerHTML = "Mobilarma 789 (Corrosion Inhibitor)";
+document.querySelectorAll(".item8a a")[0].setAttribute("href","");
+document.querySelectorAll(".item8a a")[1].setAttribute("href","https://drive.google.com/file/d/1hjJ_rQk8gp9pbnSJElge6xOQnOAF2H-T/view?usp=share_link");
+
+document.querySelector(".item8b .fw-bold").innerHTML = "Refined Glycerine";
+document.querySelectorAll(".item8b a")[0].setAttribute("href","");
+document.querySelectorAll(".item8b a")[1].setAttribute("href","https://drive.google.com/file/d/1mTkdNOZw8KbaEFhaAKKuEvHv1vv-R_nA/view?usp=share_link");
+
+document.querySelector(".item8c .fw-bold").innerHTML = "YUSHIRO Nonruster ACR (Rust Preventive Agent)";
+document.querySelectorAll(".item8c a")[0].setAttribute("href","https://drive.google.com/file/d/1mssR8ya1XgWkoklPDp0oPIaB21cKwBde/view?usp=share_link");
+document.querySelectorAll(".item8c a")[1].setAttribute("href","https://drive.google.com/file/d/1qq3y09YAXn51d9cTip33TfVfHKHgMXO3/view?usp=share_link");
+
+document.querySelector(".item8d .fw-bold").innerHTML = "FUTURECLEAN Rust Remover (Rust Remover)";
+document.querySelectorAll(".item8d a")[0].setAttribute("href","");
+document.querySelectorAll(".item8d a")[1].setAttribute("href","https://drive.google.com/file/d/1AAVFgn9OReXNOHZH1ues9HASREba6zYz/view?usp=share_link");
+
+document.querySelector(".item8e .fw-bold").innerHTML = "SWAGELOK Snoop (Soap Solution)";
+document.querySelectorAll(".item8e a")[0].setAttribute("href","https://drive.google.com/file/d/1eqdhQKJLaPPLocPeYfb0Q2ryjaDydHu3/view?usp=share_link");
+document.querySelectorAll(".item8e a")[1].setAttribute("href","https://drive.google.com/file/d/1NRzeDtxQY8XhuV_pA-FVIkPJqAaec2PX/view?usp=share_link");
+
+document.querySelector(".item8f .fw-bold").innerHTML = "Weldbrite (Weld Scale Remover)";
+document.querySelectorAll(".item8f a")[0].setAttribute("href","https://drive.google.com/file/d/1IocPabuZZDCprhNQGVyO4kd1ErhgxpRU/view?usp=share_link");
+document.querySelectorAll(".item8f a")[1].setAttribute("href","https://drive.google.com/file/d/1Ebyy8zph0HF1nd7BkJFs4FLX-oP4gRHx/view?usp=share_link");
+
+
+
